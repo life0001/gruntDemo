d81ea5ac15d785ca52f223f9db9b34908b609691
Patrick <life0001@126.com>
2016-06-23 14:31:22 +0800

    Update Gruntfile.js

--- grunt实例/Gruntfile.js
+++ grunt实例/Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     // 项目配置
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'), //grunt.file.readJSON会将我们的配置文件读出，并且转换为json对象,方便下面调用pkg.xxx
-        uglify: {  //uglify是一个插件（压缩）
+        uglify: {  //Node模块 用于JS文件压缩与合并
             //options: {
             //    banner: '/*! <%= pkg.file %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             //},
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
                 }
             }
         },
-        cssmin: {  //css文件压缩与合并
+        cssmin: {  //Node模块 用于css文件压缩与合并
           compress: {
             files: {
               'dest/1.min.css': //生成后的压缩文件名和路径
@@ -31,8 +31,9 @@ module.exports = function (grunt) {
           }
         }
     });
-    // 加载提供"uglify"任务的插件
-    grunt.loadNpmTasks('grunt-contrib-uglify');  //加载uglify（压缩） 插件
-    // 默认任务
-    grunt.registerTask('default', ['uglify']);    //最后注册一个自定义任务（其实也是默认任务）
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');  //加载uglify模块（压缩与合并JS） 
+    grunt.loadNpmTasks('grunt-contrib-cssmin');  //加载cssmin模块（压缩与合并CSS）
+    // 注册任务
+    grunt.registerTask('default', ['uglify','cssmin']);
 }
